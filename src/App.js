4ee48b96e52e454199a0133e55c56dfa6a5fd922
Edgar Salinas <EdgarHSalinas@gmail.com>
2018-07-05 15:53:20 -0700

    Added Stateless Component GuestList with propTypes array isRequired

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import GuestList from './GuestList';
 
 class App extends Component {
+  
+  state =  {
+    guests: [
+      {
+        name: 'Edgar Salinas',
+        isConfirmed: false
+      },
+      {
+        name: 'React RSVP',
+        isConfirmed: true
+      },
+      {
+        name: 'Create App',
+        isConfirmed: false
+      }
+    ]
+  }
+
+  getTotalInvited = () => this.state.guests.length;
+
+  // To Do
+  // getAttendingGuests = () =>
+  // getUnconfirmedGuests = () =>
   render() {
     return (
     <div className="App">
@@ -36,32 +60,9 @@ class App extends Component {
             </tr>
           </tbody>
         </table>
-        <ul>
-          <li className="pending"><span>Safia</span></li>
-          <li className="responded"><span>Iver</span>
-            <label>
-              <input type="checkbox" checked /> Confirmed
-            </label>
-            <button>edit</button>
-            <button>remove</button>
-          </li>
-          <li className="responded">
-            <span>Corrina</span>
-            <label>
-              <input type="checkbox" checked /> Confirmed
-            </label>
-            <button>edit</button>
-            <button>remove</button>
-          </li>
-          <li>
-            <span>Joel</span>
-            <label>
-              <input type="checkbox" /> Confirmed
-            </label>
-            <button>edit</button>
-            <button>remove</button>
-          </li>
-        </ul>
+
+      <GuestList guests={this.state.guests}/>
+
       </div>
     </div>
     );
